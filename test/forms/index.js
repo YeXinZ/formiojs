ab1af94f28e1c200eaa6c44297a483b0a3e96ec3
yexinzhang <yexin.zhang@ebistrategy.com>
2019-12-30 15:25:33 +0800

    first commit

---
+++ test/forms/index.js
@@ -0,0 +1,21 @@
+import Simple from './simple';
+import Conditions from './conditions';
+import Calculated from './calculatedfields';
+import FieldLogic from './fieldLogic';
+import ClearOnHide from './clearOnHide';
+import ClearOnHide2 from './clearOnHide2';
+import SubmissionSetter from './submissionSetter';
+import NestedFormValidation from './nested-form-validation';
+import NestedFormTests from './nested-form-tests';
+
+export default [
+  Simple,
+  SubmissionSetter,
+  Conditions,
+  Calculated,
+  FieldLogic,
+  ClearOnHide,
+  ClearOnHide2,
+  NestedFormValidation,
+  NestedFormTests,
+];
